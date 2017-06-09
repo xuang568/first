8eb01bfb5217dfeb035c8d027efa011f432ffaca
ang <ang@ANG>
2017-06-09 16:17:17 +0800

    del sum()

--- gittset/src/tset/Ta.java
+++ gittset/src/tset/Ta.java
@@ -1,10 +1,7 @@
 package tset;
 
 public class Ta {
-	public int sum(int x,int y){
-		System.out.println(x+y);
-		return x+y;
-	}
+	
 	public void print(){}
 	public static void main(String[] args) {     
 	
