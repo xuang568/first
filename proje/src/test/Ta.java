32e48d3212d2f7b1cd1c09cb408768f1344ba911
ang <ang@ANG>
2017-06-09 17:09:12 +0800

    s2

--- proje/src/test/Ta.java
+++ proje/src/test/Ta.java
@@ -2,9 +2,10 @@ package test;
 
 public class Ta {
 
-	public void print(){}
+	
 	public static void main(String[] args) {
-	System.out.println("haha");
+		System.out.println("haha");
+		System.out.println();
 
 	}
 
