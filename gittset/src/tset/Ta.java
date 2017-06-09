12577652e021a5db6e8e80a924e6fd85f2cb25b7
ang <ang@ANG>
2017-06-09 11:02:37 +0800

    去sum()

--- gittset/src/tset/Ta.java
+++ gittset/src/tset/Ta.java
@@ -2,10 +2,12 @@ package tset;
 
 public class Ta {
 	public int sum(int x,int y){
+		System.out.println(x+y);
 		return x+y;
 	}
 	public static void main(String[] args) {
-		
+		Ta ta=new Ta();
+		ta.sum(33, 55);
 	}
 }
 
