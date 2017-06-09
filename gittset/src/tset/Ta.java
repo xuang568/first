801a85660319d6c8b72b93fae4828b80dc3a2051
ang <ang@ANG>
2017-06-09 16:07:46 +0800

    222

--- gittset/src/tset/Ta.java
+++ gittset/src/tset/Ta.java
@@ -1,14 +1,11 @@
 package tset;
 
+
 public class Ta {
-	public int sum(int x,int y){
-		
-		return x+y;
-	}
-	public void print(){}
-	public static void main(String[] args) {     
+
+
+	public static void main(String[] args) {
+
 	
-		
 	}
 }
-
--- proje/src/test/Ta.java
+++ proje/src/test/Ta.java
@@ -4,8 +4,7 @@ public class Ta {
 
 	public void print(){}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
 	}
 
 }
